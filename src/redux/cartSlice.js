c88ab95bde87d0868b1d2dbd9000a985c894a5ce
Subham Saha <saha007subham@gmail.com>
2023-05-09 23:20:20 +0530

    created redux store

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+
+  reducers: {
+    addItems: (state, action) => {
+      state.items.push(action.payload);
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+  },
+});
+
+export const { addItems, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
